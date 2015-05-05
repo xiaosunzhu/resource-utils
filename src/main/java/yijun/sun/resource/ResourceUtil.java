9d6a3aea79ad51a92b3bd38f50a5ad7991f0567c
sunyijun <xun.yj@30raycom.com>
2015-05-05 17:26:29 +0800

    Add resource copy/get absolute path base util. Add unicode io util.

---
+++ src/main/java/yijun/sun/resource/ResourceUtil.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2015 yijun.sun
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package yijun.sun.resource;
+
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+
+/**
+ * Copy resources to file system.<br/>
+ *
+ * @author yijun.sun
+ * @since 0.0.1
+ */
+public class ResourceUtil {
+
+    /**
+     * Copy resources to file system.
+     *
+     * @param resourceAbsoluteClassPath resource's absolute class path, start with "/"
+     * @param targetFile                target file
+     * @throws java.io.IOException
+     */
+    public static void copyResourceToFile(String resourceAbsoluteClassPath, File targetFile) throws IOException {
+        InputStream is = ResourceUtil.class.getResourceAsStream(resourceAbsoluteClassPath);
+        if (is == null) {
+            throw new IOException("Resource not found! " + resourceAbsoluteClassPath);
+        }
+        OutputStream os = null;
+        try {
+            os = new FileOutputStream(targetFile);
+            byte[] buffer = new byte[2048];
+            int length;
+            while ((length = is.read(buffer)) != -1) {
+                os.write(buffer, 0, length);
+            }
+            os.flush();
+        } finally {
+            try {
+                is.close();
+                if (os != null) {
+                    os.close();
+                }
+            } catch (Exception ignore) {
+                // ignore
+            }
+        }
+    }
+
+    public static String getAbsolutePath(String classPath) {
+        URL configUrl = ResourceUtil.class.getResource(classPath);
+        if (configUrl == null) {
+            return null;
+        }
+        try {
+            return configUrl.toURI().getPath();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
---
+++ src/main/java/yijun/sun/resource/UnicodeReader.java
@@ -0,0 +1,136 @@
+package yijun.sun.resource;
+
+/***
+ Base on:
+ version: 1.1 / 2007-01-25
+ - changed BOM recognition ordering (longer boms first)
+ Original pseudocode   : Thomas Weidenfeller
+ Implementation tweaked: Aki Nieminen
+ http://www.unicode.org/unicode/faq/utf_bom.html
+ BOMs:
+ 00 00 FE FF    = UTF-32, big-endian
+ FF FE 00 00    = UTF-32, little-endian
+ EF BB BF       = UTF-8,
+ FE FF          = UTF-16, big-endian
+ FF FE          = UTF-16, little-endian
+ Win2k Notepad:
+ Unicode format = UTF-16LE
+
+ Update by: yijun.sun 2015-05-04
+ ***/
+
+import java.io.*;
+
+/**
+ * Generic unicode textReader, which will use BOM mark
+ * to identify the encoding to be used. If BOM is not found
+ * then use a given default or system encoding.
+ * Usage pattern:
+ * String enc = "ISO-8859-1"; // or NULL to use systemDefault
+ * FileInputStream fis = new FileInputStream(file);
+ * UnicodeReader uReader = new UnicodeReader(fis, enc);
+ * enc = uReader.getEncoding(); // check and skip possible BOM bytes
+ * or:
+ * UnicodeReader uin = new UnicodeReader(fis, enc).skipBOM();
+ * <p/>
+ *
+ * @since 0.0.1
+ */
+public class UnicodeReader extends Reader {
+    PushbackInputStream internalIn;
+    InputStreamReader internalIn2 = null;
+    String defaultEnc;
+    private static final int BOM_SIZE = 4;
+
+    /**
+     * @param in inputStream to be read
+     */
+    public UnicodeReader(InputStream in) {
+        internalIn = new PushbackInputStream(in, BOM_SIZE);
+        this.defaultEnc = null;
+    }
+
+    /**
+     * @param in         inputStream to be read
+     * @param defaultEnc default encoding if stream does not have
+     *                   BOM marker. Give NULL to use system-level default.
+     */
+    public UnicodeReader(InputStream in, String defaultEnc) {
+        internalIn = new PushbackInputStream(in, BOM_SIZE);
+        this.defaultEnc = defaultEnc;
+    }
+
+    public UnicodeReader skipBOM() {
+        getEncoding();
+        return this;
+    }
+
+    public String getDefaultEncoding() {
+        return defaultEnc;
+    }
+
+    /**
+     * Get stream encoding or NULL if stream is uninitialized.
+     * Call init() or read() method to initialize it.
+     */
+    public String getEncoding() {
+        if (internalIn2 == null)
+            return null;
+        return internalIn2.getEncoding();
+    }
+
+    /**
+     * Read-ahead four bytes and check for BOM marks. Extra bytes are
+     * unread back to the stream, only BOM bytes are skipped.
+     */
+    protected void init() throws IOException {
+        if (internalIn2 != null)
+            return;
+        String encoding;
+        byte bom[] = new byte[BOM_SIZE];
+        int n, unread;
+        n = internalIn.read(bom, 0, bom.length);
+        if ((bom[0] == (byte) 0x00) && (bom[1] == (byte) 0x00) &&
+                (bom[2] == (byte) 0xFE) && (bom[3] == (byte) 0xFF)) {
+            encoding = "UTF-32BE";
+            unread = n - 4;
+        } else if ((bom[0] == (byte) 0xFF) && (bom[1] == (byte) 0xFE) &&
+                (bom[2] == (byte) 0x00) && (bom[3] == (byte) 0x00)) {
+            encoding = "UTF-32LE";
+            unread = n - 4;
+        } else if ((bom[0] == (byte) 0xEF) && (bom[1] == (byte) 0xBB) &&
+                (bom[2] == (byte) 0xBF)) {
+            encoding = "UTF-8";
+            unread = n - 3;
+        } else if ((bom[0] == (byte) 0xFE) && (bom[1] == (byte) 0xFF)) {
+            encoding = "UTF-16BE";
+            unread = n - 2;
+        } else if ((bom[0] == (byte) 0xFF) && (bom[1] == (byte) 0xFE)) {
+            encoding = "UTF-16LE";
+            unread = n - 2;
+        } else {
+            // Unicode BOM mark not found, unread all bytes
+            encoding = defaultEnc;
+            unread = n;
+        }
+        //System.out.println("read=" + n + ", unread=" + unread);
+        if (unread > 0)
+            internalIn.unread(bom, (n - unread), unread);
+        // Use given encoding
+        if (encoding == null) {
+            internalIn2 = new InputStreamReader(internalIn);
+        } else {
+            internalIn2 = new InputStreamReader(internalIn, encoding);
+        }
+    }
+
+    public void close() throws IOException {
+        init();
+        internalIn2.close();
+    }
+
+    public int read(char[] cBuf, int off, int len) throws IOException {
+        init();
+        return internalIn2.read(cBuf, off, len);
+    }
+}
